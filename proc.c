8a5bd162e68ec161e754a1f44d30202ad2b0dd8e
whb <295851252@qq.com>
2019-09-23 06:22:22 -0700

    这是我的第一次提交，代码用于测试

---
+++ proc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void proc()
+{
+    const char *lable = "|/-\\";
+    int i = 0;
+    char bar[101];
+    memset(bar, '\0', sizeof(bar));
+
+    for(; i <= 100; i++)
+    {
+        printf("[%-100s][%d%%][%c]\r", bar, i, lable[i%4]);
+        bar[i] = '#';
+        fflush(stdout);
+        usleep(100000);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    proc();
+    return 0;
+}
